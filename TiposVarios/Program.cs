1b9a061dbb314c5161e2ba79c03ea038b963ca65
Daniel <danielmordelfer@gmail.com>
2023-03-03 00:33:23 +0000

    new project TiposVarios

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+    
+        static void Main(string[] args)
+        {
+         sbyte x1 = 127;
+         short x2 = 32767;
+         byte x3 = 255;
+            Console.WriteLine("x1 = "+ x1.ToString());
+            Console.WriteLine("x2 = "+ x2.ToString());
+            Console.WriteLine("x3 = "+ x3.ToString());
+        }
+    }
+}
