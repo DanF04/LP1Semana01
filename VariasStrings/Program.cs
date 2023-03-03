fb556eeb75e2842164703e26d725b0c9fc12d536
Daniel <danielmordelfer@gmail.com>
2023-03-03 01:01:52 +0000

    new project VariasStrings

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "|\u266a\t|\u250b\t|\u263c\t|\n|u266a\t|u250b\t|u263c\t|";
+            string s2 = @"|\u266a\t|\u250b\t|\u263c\t|\n|u266a\t|u250b\t|u263c\t|";
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+        }
+    }
+}
