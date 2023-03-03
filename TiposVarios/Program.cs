129661b32b9e5a9a92d6b009ecff2d179c591591
Daniel <danielmordelfer@gmail.com>
2023-03-03 00:41:25 +0000

    TiposVarios char

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -7,12 +7,21 @@ namespace TiposVarios
     
         static void Main(string[] args)
         {
-         sbyte x1 = 127;
-         short x2 = 32767;
-         byte x3 = 255;
+            sbyte x1 = 127;
+            short x2 = 32767;
+            byte x3 = 255;
             Console.WriteLine("x1 = "+ x1.ToString());
             Console.WriteLine("x2 = "+ x2.ToString());
             Console.WriteLine("x3 = "+ x3.ToString());
+
+
+            char y1 = '\u270d';
+            char y2 = '\u240e';
+            char y3 = '\u270f';
+            Console.WriteLine("y1 = "+ y1);
+            Console.WriteLine("y2 = "+ y2);
+            Console.WriteLine("y3 = "+ y3);
+            
         }
     }
 }
