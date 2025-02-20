962a770b1ad1652f32e28e25d56f64efc2fcfd51
Daniel <danielmordelfer@gmail.com>
2025-02-20 18:27:52 +0000

    Added Variables for whole numbers

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Variables for numbers
+            sbyte I = 111;
+            short A = 4444;
+            int B = 888888888;
+            long S = 5555555555555555555;
+            
+            //Printing the respective variables
+            Console.WriteLine (I);
+            Console.WriteLine (A);
+            Console.WriteLine (B);
+            Console.WriteLine (S);
+        }
+    }
+}
