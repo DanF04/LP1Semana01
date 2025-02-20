5375b363a20cf1ce52cdf34ba15594ea7663bd7e
Daniel <danielmordelfer@gmail.com>
2025-02-20 18:44:23 +0000

    Added Variables for characters

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -6,17 +6,27 @@ namespace VariousTypes
     {
         private static void Main(string[] args)
         {
-            //Variables for numbers
+            //Variables for whole numbers
             sbyte I = 111;
             short A = 4444;
             int B = 888888888;
             long S = 5555555555555555555;
-            
+
             //Printing the respective variables
             Console.WriteLine (I);
             Console.WriteLine (A);
             Console.WriteLine (B);
             Console.WriteLine (S);
+
+            //Variables for characters
+            char heart = '\u10E6';
+            char star = '\u2730';
+            char comma = '\u275B';
+            
+            //Printing the respective characters
+            Console.WriteLine (heart);
+            Console.WriteLine (star);
+            Console.WriteLine (comma);
         }
     }
 }
