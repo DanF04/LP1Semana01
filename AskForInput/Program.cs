ecb77b75c72c024d9382314bf2a8e1400cca5d95
Daniel <danielmordelfer@gmail.com>
2025-02-21 01:55:42 +0000

    Added variables that permit the usar to write another input and also transform that input into a real number

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -6,11 +6,16 @@ namespace AskForInput
     {
         private static void Main(string[] args)
         {
-            //Variable to ask for input from the user
+            //Variables to ask for input from the user
             string ask = Console.ReadLine();
+            string askreal = Console.ReadLine();
 
-            //Variable that converts the user input into a whole number"
+            //Variable that converts the user input into a whole number
             int number = int.Parse(ask);
+
+            //Variable that converts the user input into a real number
+            float realnumber = float.Parse(askreal);
+
 
         }
     }
