057da78bddc0441ce5dd4c870a20f5184fb8afda
Daniel <danielmordelfer@gmail.com>
2025-02-21 00:36:03 +0000

    Added variables for strings

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Variables for strings
+            string notifier = "Hello LP1";
+            string hidden = "He is \"hidden\" among us...";
+            string ismoney = "this \u20AC is money";
+
+            //Printing the respective strings
+            Console.WriteLine (notifier);
+            Console.WriteLine (hidden);
+            Console.WriteLine (ismoney);
+        }
+    }
+}
