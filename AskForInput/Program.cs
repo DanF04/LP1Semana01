481b02f61b35af9003ca0a7adcbe9be72d51fc37
Daniel <danielmordelfer@gmail.com>
2025-02-21 01:52:29 +0000

    Added variables that permit the usar to write an input and also transform that input into a whole number

---
+++ AskForInput/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AskForInput
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Variable to ask for input from the user
+            string ask = Console.ReadLine();
+
+            //Variable that converts the user input into a whole number"
+            int number = int.Parse(ask);
+
+        }
+    }
+}
