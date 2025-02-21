eedccb52f69738a4b0a13fc88287b43c5e43e407
Daniel <danielmordelfer@gmail.com>
2025-02-21 02:05:35 +0000

    Added a variable that adds the two user inputs and also the printing of the results

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -16,6 +16,16 @@ namespace AskForInput
             //Variable that converts the user input into a real number
             float realnumber = float.Parse(askreal);
 
+            //Variable that adds the two inputs together
+            float AddInputs = number + realnumber;
+            
+
+            Console.WriteLine ("Write a whole number:"+ ask);
+            Console.WriteLine ("Write a real number:"+ askreal);
+            Console.WriteLine ($"{number} + {realnumber} = {AddInputs}");
+
+
+
 
         }
     }
