1e82bbac1771b19386e8b3c5924c4952d43ff4f5
Daniel <danielmordelfer@gmail.com>
2025-02-21 00:57:09 +0000

    Added variables for connected and interpolated strings

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -7,14 +7,19 @@ namespace ManyStrings
         private static void Main(string[] args)
         {
             //Variables for strings
-            string notifier = "Hello LP1";
+            string notifier = "strings";
             string hidden = "He is \"hidden\" among us...";
             string ismoney = "this \u20AC is money";
+            string evenmorestrings = "this" + "adds" + 4 + "more" + notifier;
+            string x = "x" + "\u00B2";
+            string mathematics =  $"x times x is equal to {x}";
 
             //Printing the respective strings
             Console.WriteLine (notifier);
             Console.WriteLine (hidden);
             Console.WriteLine (ismoney);
+            Console.WriteLine (evenmorestrings);
+            Console.WriteLine (mathematics);
         }
     }
 }
